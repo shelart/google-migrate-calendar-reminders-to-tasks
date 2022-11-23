2461e0e90ddd0e6647c8f615b493459dcd308c45
Arturas <shelekhov.arthur@gmail.com>
2022-11-23 19:29:57 +0200

    Introduce yargs

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers';
 import auth from './auth/request';
 
-await auth();
+const args = yargs(hideBin(process.argv))
+    .command('auth', 'Perform OAuth 2.0 flow and store the token for this application.', auth)
+    .help()
+    .argv;
