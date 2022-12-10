216067424eb96f3879b96dd6ab0f9bcc64d961c6
Arturas <shelekhov.arthur@gmail.com>
2022-12-10 19:30:40 +0200

    Add options to control loading of Google Reminders

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,47 @@ import fs from 'fs';
 
 const args = yargs(hideBin(process.argv))
     .command('auth', 'Perform OAuth 2.0 flow and store the token for this application.', authenticate)
-    .command('prepare', 'Download reminders from Google Calendar and generate prepared.json file.', async () => {
-        const reminders = await getReminders(
-            new Date(2016, 0, 1),
-            new Date(new Date().getTime() + 365 * 24 * 3600 * 1000), // 1 year after now
-            30 * 24 * 3600 * 1000 // step 30 days
-        );
-        console.log(`Preparing ${reminders.length} reminders...`);
-        fs.writeFileSync('prepared.json', JSON.stringify(reminders, null, 2));
-    })
+    .command('prepare', 'Download reminders from Google Calendar and generate prepared.json file.',
+        yargs => {
+            const tillDefault = new Date(new Date().getTime() + 365 * 24 * 3600 * 1000); // 1 year after now
+            const tillDefaultStr = `${tillDefault.getFullYear()}-${tillDefault.getMonth() + 1}-${tillDefault.getDate()}`;
+
+            return yargs.options({
+                'from': {
+                    type: 'string',
+                    default: '2016-01-01',
+                    description: 'Starting date for a loop, within which Google API would be requested to return all reminders created before an iterated date.'
+                        + ' Must be in format: YYYY-MM-DD.',
+                },
+                'till': {
+                    type: 'string',
+                    default: tillDefaultStr,
+                    description: 'Ending date for a loop, within which Google API would be requested to return all reminders created before an iterated date.'
+                        + ' Must be in format: YYYY-MM-DD.',
+                },
+                'step': {
+                    type: 'number',
+                    default: 30,
+                    description: 'Step (in days) for a loop, within which Google API would be requested to return all reminders created before an iterated date.',
+                },
+            });
+        },
+        async (argv) => {
+            const parseDate = (dateStr: string) => {
+                const dateComponents = dateStr.split('-');
+                const year = parseInt(dateComponents[0], 10);
+                const month = parseInt(dateComponents[1], 10);
+                const dayOfMonth = parseInt(dateComponents[2], 10);
+                return new Date(year, month - 1, dayOfMonth);
+            };
+
+            const reminders = await getReminders(
+                parseDate(argv.from),
+                parseDate(argv.till),
+                argv.step * 24 * 3600 * 1000
+            );
+            console.log(`Preparing ${reminders.length} reminders...`);
+            fs.writeFileSync('prepared.json', JSON.stringify(reminders, null, 2));
+        })
     .help()
     .argv;
