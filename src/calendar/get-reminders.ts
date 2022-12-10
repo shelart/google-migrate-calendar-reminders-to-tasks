faa4e4197588d3908f3b8fec398582d7daf543bd
Arturas <shelekhov.arthur@gmail.com>
2022-12-10 18:54:58 +0200

    Fix loading of Google Calendar Reminders to load them all (as possible)

--- src/calendar/get-reminders.ts
+++ src/calendar/get-reminders.ts
@@ -6,10 +6,9 @@ import loadAuthToken from '../auth/load-auth-token';
 import axios from 'axios';
 import {GoogleProtobufRemindersList} from './_google-protobuf-reminders-list-type';
 import {Reminder} from './reminder-type';
+import {Token} from '../auth/token-type';
 
-export default async function(): Promise<Reminder[]> {
-    const authToken = await loadAuthToken();
-
+const loadRemindersCreatedBefore = async function(authToken: Token, createdBefore: Date): Promise<Reminder[]> {
     let responseBody, httpStatus;
     try {
         const {data, status} = await axios.post<GoogleProtobufRemindersList>(
@@ -17,6 +16,7 @@ export default async function(): Promise<Reminder[]> {
             JSON.stringify({
                 '5': 1,
                 '6': 9999,
+                '16': createdBefore.getTime() + 15 * 3600 * 1000,
             }),
             {
                 params: {
@@ -46,4 +46,24 @@ export default async function(): Promise<Reminder[]> {
     } else {
         return protobufRemindersList['1']!.map(protobufReminder => new Reminder(protobufReminder));
     }
+}
+
+export default async function(from: Date, till: Date, stepMillis: number): Promise<Reminder[]> {
+    const authToken = await loadAuthToken();
+    let result: { [id: string] : Reminder } = {};
+    for (
+        let loadBefore = from;
+        loadBefore.getTime() <= till.getTime();
+        loadBefore = new Date(loadBefore.getTime() + stepMillis)
+    ) {
+        console.log(`Loading reminders created before ${loadBefore}...`);
+        const remindersPage = await loadRemindersCreatedBefore(authToken, loadBefore);
+        const oldCount = Object.entries(result).length;
+        for (const reminder of remindersPage) {
+            result[reminder.id] = reminder;
+        }
+        const newCount = Object.entries(result).length;
+        console.log(`Loaded ${remindersPage.length} reminders (only ${newCount - oldCount} were not loaded before).`);
+    }
+    return Object.values(result);
 }
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ import fs from 'fs';
 const args = yargs(hideBin(process.argv))
     .command('auth', 'Perform OAuth 2.0 flow and store the token for this application.', authenticate)
     .command('prepare', 'Download reminders from Google Calendar and generate prepared.json file.', async () => {
-        const reminders = await getReminders();
+        const reminders = await getReminders(
+            new Date(2016, 0, 1),
+            new Date(new Date().getTime() + 365 * 24 * 3600 * 1000), // 1 year after now
+            30 * 24 * 3600 * 1000 // step 30 days
+        );
+        console.log(`Preparing ${reminders.length} reminders...`);
         fs.writeFileSync('prepared.json', JSON.stringify(reminders, null, 2));
     })
     .help()
